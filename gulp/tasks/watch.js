ea3ba3e9b7f8bd6cd382134fd1cdb59a8ef80ab6
Arturo Zarzalejo <Arturoazh@gmail.com>
2016-05-23 12:27:18 +0200

    fix()

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -6,7 +6,7 @@ gulp.task('watch', ['_watch']);
 gulp.task('_watch', function() {
   // gulp.watch(config.markup.src, ['build-css', 'build-html', 'build-js']);
   gulp.watch(config.jsFiles, ['build-js']);
-  gulp.watch(config.scssFiles, ['build-css']);
+  // gulp.watch(config.scssFiles, ['build-css']);
   gulp.watch(config.htmlFiles, ['build-html']);
   gulp.watch(config.otherFiles, ['copy-other']);
 });
