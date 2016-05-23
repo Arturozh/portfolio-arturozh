f95073fa6b3df6c2f0329be3cb8d39a8e759a219
Arturo Zarzalejo <Arturoazh@gmail.com>
2016-05-23 12:28:53 +0200

    fix()
    scss

--- gulp/tasks/build-css.js
+++ gulp/tasks/build-css.js
@@ -1,19 +1,19 @@
-'use strict';
-
-console.log('entra aquí¿?');
-var config     = require('../config');
-var gulp       = require('gulp');
-var sass       = require('gulp-sass');
-var notify     = require('gulp-notify');
-var concat     = require('gulp-concat');
-var minifyCSS  = require('gulp-minify-css');
-
-gulp.task('build-css', function () {
-  gulp.src(config.scssFiles)
-    .pipe(concat('styles.min.css'))
-    .pipe(sass())
-    .on("error", notify.onError("Error: <%= error.message %>"))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest(config.markup.dest+'/css/'))
-    .pipe(notify("CSS Procesado"));
-  });
+// 'use strict';
+//
+// console.log('entra aquí¿?');
+// var config     = require('../config');
+// var gulp       = require('gulp');
+// var sass       = require('gulp-sass');
+// var notify     = require('gulp-notify');
+// var concat     = require('gulp-concat');
+// var minifyCSS  = require('gulp-minify-css');
+//
+// gulp.task('build-css', function () {
+//   gulp.src(config.scssFiles)
+//     .pipe(concat('styles.min.css'))
+//     .pipe(sass())
+//     .on("error", notify.onError("Error: <%= error.message %>"))
+//     .pipe(minifyCSS())
+//     .pipe(gulp.dest(config.markup.dest+'/css/'))
+//     .pipe(notify("CSS Procesado"));
+//   });
